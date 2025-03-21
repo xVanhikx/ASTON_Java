7cdc6d113d7f14a394b61bf777775ef28d3efa67
Vadzim <shylkovad@gmail.com>
2025-03-21 18:41:24 +0300

    добавил Comparator в классы entity

--- src/main/java/ru/aston/entity/Car.java
+++ src/main/java/ru/aston/entity/Car.java
@@ -4,12 +4,22 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Comparator;
+
 @AllArgsConstructor
 @Data
 @NoArgsConstructor
-public class Car {
+public class Car implements Comparable<Car> {
 
     private Integer power;
     private String model;
     private Integer year;
+
+    @Override
+    public int compareTo(Car other) {
+        return Comparator.comparing(Car::getPower)
+                .thenComparing(Car::getModel)
+                .thenComparing(Car::getYear)
+                .compare(this, other);
+    }
 }
--- src/main/java/ru/aston/entity/RootVegetable.java
+++ src/main/java/ru/aston/entity/RootVegetable.java
@@ -4,12 +4,22 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Comparator;
+
 @AllArgsConstructor
 @Data
 @NoArgsConstructor
-public class RootVegetable {
+public class RootVegetable implements Comparable<RootVegetable> {
 
     private String type;
     private Integer weight;
     private String color;
+
+    @Override
+    public int compareTo(RootVegetable other) {
+        return Comparator.comparing(RootVegetable::getType)
+                .thenComparing(RootVegetable::getWeight)
+                .thenComparing(RootVegetable::getColor)
+                .compare(this, other);
+    }
 }
